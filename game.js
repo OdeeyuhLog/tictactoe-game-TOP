4dd56ff82814965886d4f0fce6329ebd15582854
Koni <konichan619@gmail.com>
2022-08-15 16:34:37 +0800

    Add comments in js file for better documentation

--- game.js
+++ game.js
@@ -6,15 +6,18 @@
       this.bindEvents();
     },
 
+    // Cache for the DOM elements involved
     allDOM: function () {
       this.startBtn = document.getElementById('start-app');
       this.landPage = document.querySelector('.start-page');
     },
 
+    // Event listeners
     bindEvents: function () {
       this.startBtn.addEventListener('click', this.startApp.bind(this));
     },
 
+    // Removes the landing page
     startApp: function () {
       this.landPage.classList.add('erase');
     },
